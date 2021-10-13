f1fac841483850aee0b0a20d0add20c6663531cf
Luong-Cong-Duy97 <duylcong97@gmail.com>
2021-10-13 10:37:59 +0700

    add validates form

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,8 @@
 class Friend < ApplicationRecord
 	belongs_to :user
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, presence: true
+	validates :phone, presence: true
+	validates :twitter, presence: true
 end
